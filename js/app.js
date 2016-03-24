5dc7e83cd9139b0c7285e07e5a36919b5ede0dff
Victor <viclee02@gmail.com>
2016-03-23 21:56:31 -1000

    refactor

--- js/app.js
+++ js/app.js
@@ -1,51 +1,59 @@
 window.onload = function(){
 
+ console.log('test');
 
- var items = [
+ var products = [
   { "name": "Lusicious Jello Mix", "description": ["Very Elegant", "Trending item", "Come in Purple"], "price": 80.65 },
   { "name": "Tarnished Standing Desk", "description": ["Modular", "Works for both Tall and Loud People", "Smells like Productivity"], "price": 1654.99},
   { "name": "Hand-made Hand Grenades", "description": ["Such gift!", "Much boom!", "Very safe for kids"], "price": 10.44},
   { "name": "Pan-fried Cookie Dough", "description": ["Chocolate", "Family-size", "Hot Mess"], "price": 16.99 },
   { "name": "Fancy Dress Hanger", "description": ["Keep organized", "On Sale"], "price": 67.32 },
   { "name": "Snarky Britsh Mustache 3-Pack", "description": ["Sharing is caring!", "Hugs not drugs", "As seen on 'So You Think You Can Dance - Nigeria!'"], "price": 1.99 },
- ];
-   
-   for(var i = 0; i<items.length; i++){
-    console.log(items[i].name);
-    console.log(items[i].description);
-    console.log(items[i].description[0]);
-    console.log(items[i].price);
-    
-    var list = items[i];
-    var nameList = list.name;
-    var descriptList = list.description;
-    var priceList = list.price;
+];
+   function getItems (stuff){
+    for (var i = 0; i<stuff.length; i++){
+      console.log(stuff[i]);
+      console.log(stuff[i].name);
+      console.log(stuff[i].description);
+      console.log(stuff[i].price);
 
-    var mainElement = document.createElement('div');
-    mainElement.id = 'main';
-    document.body.appendChild(mainElement);
+      var mainContainer = document.getElementById('mainWrap');
+      mainContainer = document.createElement('div');
+      mainContainer.id = 'main';
+      mainWrap.appendChild(mainContainer);
 
-    var nameElement = document.createElement('div');
-    nameElement.id = 'name';
-    nameElement.innerHTML = nameList;
-    mainElement.appendChild(nameElement);
+      var nameElement = document.createElement('div');
+      nameElement.id = 'name';
+      nameElement.innerHTML = stuff[i].name;
+      mainContainer.appendChild(nameElement);
 
-    var descripElement = document.createElement('div');
-    descripElement.id = 'descrip';
-    descripElement.innerHTML = descriptList;
-    nameElement.appendChild(descripElement);
+      var descripElement = document.createElement('div');
+      descripElement.id = 'description';
+      descripElement.innerHTML = stuff[i].description;
+      nameElement.appendChild(descripElement);
 
-    var priceElement = document.createElement('div');
-    priceElement.id = 'price';
-    priceElement.innerHTML = priceList;
-    nameElement.appendChild(priceElement);
+      var priceElement = document.createElement('div');
+      priceElement.id = 'price';
+      priceElement.innerHTML = stuff[i].price;
+      nameElement.appendChild(priceElement);
 
-
- }
+      var quantityElement = document.createElement('div');
+      quantityElement.id = 'quantity';
+      nameElement.appendChild(quantityElement);
 
 
+    }
+    return stuff;
+   }
 
+   getItems(products);
 
+   var subContainer = document.getElementById('subWrap');
+   subContainer = document.createElement('div');
+   subContainer.id = 'subtotal';
+   subContainer.innerHTML = "Subtotal";
+   subWrap.appendChild(subContainer);
 
 
 };
+
---
+++ js/practice.js
@@ -0,0 +1,126 @@
+/*window.onload = function(){
+
+  var items = [
+  { "name": "Lusicious Jello Mix", "description": ["Very Elegant", "Trending item", "Come in Purple"], "price": 80.65 },
+  { "name": "Tarnished Standing Desk", "description": ["Modular", "Works for both Tall and Loud People", "Smells like Productivity"], "price": 1654.99},
+  { "name": "Hand-made Hand Grenades", "description": ["Such gift!", "Much boom!", "Very safe for kids"], "price": 10.44},
+  { "name": "Pan-fried Cookie Dough", "description": ["Chocolate", "Family-size", "Hot Mess"], "price": 16.99 },
+  { "name": "Fancy Dress Hanger", "description": ["Keep organized", "On Sale"], "price": 67.32 },
+  { "name": "Snarky Britsh Mustache 3-Pack", "description": ["Sharing is caring!", "Hugs not drugs", "As seen on 'So You Think You Can Dance - Nigeria!'"], "price": 1.99 },
+  ];
+
+  for(var i = 0; i<items.length; i++){
+    console.log(items[i]);
+    console.log(items[i].name);
+    console.log(items[i].description);
+    console.log(items[i].price);
+  
+  var list = items[i];
+  var nameList = list.name;
+  var description = list.description;
+  var price = list.price;
+
+   var mainElement = document.getElementById('mainWrap');
+   mainElement = document.createElement('div');
+   mainElement.id = 'main';
+   mainWrap.appendChild(mainElement);
+
+   var nameElement = document.createElement('div');
+   nameElement.id = 'name';
+   nameElement.innerHTML = nameList;
+   mainElement.appendChild(nameElement);
+
+   descripElement = document.createElement('div');
+   descripElement.id = "description";
+   descripElement.innerHTML = description;
+   nameElement.appendChild(descripElement);
+
+   priceElement = document.createElement('div');
+   priceElement.id = 'price';
+   priceElement.innerHTML = price;
+   nameElement.appendChild(priceElement);
+
+   quantityElement = document.createElement('div');
+   quantityElement.id = "quantity";
+   nameElement.appendChild(quantityElement);
+
+   buttonElement = document.createElement('button');
+   buttonElement.id = 'buy';
+   buttonElement.innerHTML = "add to cart";
+   quantityElement.appendChild(buttonElement);
+
+
+
+}
+
+};*/
+
+/*window.onload = function(){
+
+ var items = [
+  { "name": "Lusicious Jello Mix", "description": ["Very Elegant", "Trending item", "Come in Purple"], "price": 80.65},
+  { "name": "Tarnished Standing Desk", "description": ["Modular", "Works for both Tall and Loud People", "Smells like Productivity"], "price": 1654.99},
+  { "name": "Hand-made Hand Grenades", "description": ["Such gift!", "Much boom!", "Very safe for kids"], "price": 10.44},
+  { "name": "Pan-fried Cookie Dough", "description": ["Chocolate", "Family-size", "Hot Mess"], "price": 16.99},
+  { "name": "Fancy Dress Hanger", "description": ["Keep organized", "On Sale"], "price": 67.32},
+  { "name": "Snarky Britsh Mustache 3-Pack", "description": ["Sharing is caring!", "Hugs not drugs", "As seen on 'So You Think You Can Dance - Nigeria!'"], "price": 1.99},
+ ];*/
+   
+  /* for(var i = 0; i<items.length; i++){
+    console.log(items[i]);
+    console.log(items[i].name);
+    console.log(items[i].description);
+    console.log(items[i].price);
+
+    var list = items[i];
+    var nameList = list.name;
+    var descriptList = list.description;
+    var priceList = list.price;
+
+    var mainElement = document.getElementById('mainWrap');
+    mainElement = document.createElement('div');
+    mainElement.id = 'main';
+    mainWrap.appendChild(mainElement);*/
+
+
+    /*var mainElement = document.createElement('div');
+    mainElement.id = 'main';
+    document.body.appendChild(mainElement);*/
+
+    //when an HTML document is loaded into a web broswer, it becomes a document object
+
+  /*  var nameElement = document.createElement('div');
+    nameElement.id = 'name';
+    nameElement.innerHTML = nameList;
+    mainElement.appendChild(nameElement);
+
+    var descripElement = document.createElement('div');
+    descripElement.id = 'descrip';
+    descripElement.innerHTML = descriptList;
+    nameElement.appendChild(descripElement);
+
+    var priceElement = document.createElement('div');
+    priceElement.id = 'price';
+    priceElement.innerHTML = priceList;
+    nameElement.appendChild(priceElement);
+
+    var quantityElement = document.createElement('div');
+    quantityElement.id = 'quantity';
+    nameElement.appendChild(quantityElement);
+    
+    var buttonElement = document.createElement('button');
+    buttonElement.id = 'menubutton';
+    buttonElement.innerHTML = "Quantity";
+    quantityElement.appendChild(buttonElement);
+
+    var menucontent = document.createElement('li');
+    menucontent.id = 'menucontent';
+    menucontent.innerHTML = "soldout";
+    quantityElement.appendChild(menucontent);
+
+  
+    }
+
+
+};
+*/
